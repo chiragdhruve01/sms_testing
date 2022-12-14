750b3dcbd43a60eeb2d9c246212eee8690117178
chiragdhruve01 <chirag@houstonsupport.com>
2022-12-14 13:12:11 +0530

    notifications added

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -68,12 +68,67 @@ class FCMPushNotifications {
     if (payload != null) {}
   }
 
+  void showxNotification(RemoteMessage message) async {
+    RemoteNotification? notification = message.notification;
+    print("message.notification" + message.notification!.body.toString());
+    AndroidNotification? android = message.notification?.android;
+    print("android" + android!.imageUrl.toString());
+    print("android" + android.link.toString());
+    if (notification != null && android != null) {
+      final largeImagePath = await Utils.downloadFile(
+          'https://images.unsplash.com/photo-1668102228964-ebc7ca0866b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+          'largeImage');
+
+      final profilePicPath = await Utils.downloadFile(
+          'https://images.unsplash.com/photo-1668102228964-ebc7ca0866b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+          'profilePic');
+
+      final styleInformation = BigPictureStyleInformation(
+        FilePathAndroidBitmap(largeImagePath!),
+        largeIcon: FilePathAndroidBitmap(profilePicPath!),
+        contentTitle: notification.title,
+        summaryText: notification.title,
+        htmlFormatSummaryText: true,
+        htmlFormatContent: true,
+      );
+
+      AndroidNotificationDetails? androidDetails = AndroidNotificationDetails(
+        'channel id',
+        'channel name',
+        channelDescription: 'Channel Description',
+        importance: Importance.max,
+        styleInformation: styleInformation,
+      );
+      var iOSDetails = IOSNotificationDetails();
+      var generalNotificationDetails = NotificationDetails(
+        android: androidDetails,
+        iOS: iOSDetails,
+      );
+      await flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        generalNotificationDetails,
+        payload: "",
+      );
+    }
+  }
+
   Future<void> init({bool initScheduled = false}) async {
     var androidInitialize =
         AndroidInitializationSettings('@mipmap/launcher_icon');
     var iOSInitialize = IOSInitializationSettings();
     var initializeSettings =
         InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+      print('Message also contained a notification: ${message.notification}');
+      print('Message also contained a title: ${message.notification!.title}');
+      print('Message also contained a body: ${message.notification!.body}');
+      ;
+      showxNotification(message);
+    });
 
     flutterLocalNotificationsPlugin.initialize(initializeSettings,
         onSelectNotification: notificationSelected);
